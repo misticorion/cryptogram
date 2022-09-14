73a51eb0559e98aed6c40581b25f536a6ad6de94
sumedhaa <mistic.orion@gmail.com>
2022-09-14 12:15:26 +0530

    Adding reverse_cipher

---
+++ reverse_cipher/decrypt.py
@@ -0,0 +1,3 @@
+cipher_text = input("Enter Cipher Text: ")
+plain_text = cipher_text[::-1]
+print(plain_text)
---
+++ reverse_cipher/encrypt.py
@@ -0,0 +1,3 @@
+plain_text = input("Enter Plain Text: ")
+cipher_text = plain_text[::-1]
+print(cipher_text) 
